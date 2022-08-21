72dad9514a48ff531c56223b1ca57db6ed8551b3
shan <qinss.pku@gmail.com>
2022-08-21 14:36:16 -0400

    PSP left singular vectors directly computed from X

--- src/generate_PSP_input.m
+++ src/generate_PSP_input.m
@@ -5,12 +5,13 @@ V = orth(randn(input_dim,input_dim));
 U = orth(randn(output_dim,output_dim));       % arbitrary orthogonal matrix to determine the projection
 % C = V*diag(input_cov_eigens)*V'/sqrt(input_dim);    % with normalized length
 C = V*diag(input_cov_eigens)*V';    % with normalized length
-Vnorm = norm(V(:,1:output_dim));
+% Vnorm = norm(V(:,1:output_dim));
 
 % generate multivariate Gaussian distribution with specified correlation
 % matrix
 X = mvnrnd(zeros(1,input_dim),C,num_sample)';
 Cx = X*X'/num_sample;      % input sample covariance matrix
+
 
 % generate some test stimuli that are not used during learning
 num_test = 1e2;
