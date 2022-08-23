860dbbebaf8e334ea0d0bfea59b26e7ab2137ffd
shan <qinss.pku@gmail.com>
2022-08-23 10:25:31 -0400

    enable different sum of eigenvaleus

--- simulation/PSP_D_Dependency.m
+++ simulation/PSP_D_Dependency.m
@@ -24,7 +24,7 @@ rho = 0.95;             % the first k component explain the majority of the fluc
 
 num_sel = 200;          % randomly selected samples used to calculate the drift and diffusion constants
 step = 10;              % store every 10 updates
-tot_iter = 5e4;  
+tot_iter = 2e4;  
 time_points = round(tot_iter/step);
 Yt = zeros(k,time_points,num_sel);
 learnRate = 0.05;       % learning rate
@@ -36,21 +36,26 @@ if strcmp(simType,'noiseAmp')
     noiseStd = noiseStd(:);
     eigens = [4.5,3,1.5,ones(1,7)*0.01];
     allDiffConst = nan(length(noiseStd),2);  % store all the diffusion constant and exponents
+    all_ave_rmsd = cell(length(noiseStd),1);  % store all the mean rmsd
     parfor i = 1:length(noiseStd)
-        [dph,ephi] = rotaDiffConst(n,k, eigens, noiseStd(i),learnRate);
+        [dph,ephi,ave_rmsd] = rotaDiffConst(n,k, eigens, noiseStd(i),learnRate);
         allDiffConst(i,:) = [dph,ephi];
-    end
+        all_ave_rmsd{i} = ave_rmsd;
+        disp(noiseStd(i));
+     end
 elseif strcmp(simType,'eigenSpectr')
     noiseStd = 0.01; 
-    eigens = nan(10,totSimul);    % store all the input eigen values
-    allDiffConst = nan(totSimul,2) ; %store all the diffusion constant and exponents
+    eigens = nan(10,totSimul);        % store all the input eigen values
+    allDiffConst = nan(totSimul,2) ;  %store all the diffusion constant and exponents
+    all_ave_rmsd = cell(totSimul,1);  % store all the mean rmsd
     parfor i= 1:totSimul
-        eigens(:,i) = SMhelper.genRandEigs(k,n,rho,'lognorm');
-        [dph,ephi] = rotaDiffConst(n,k, eigens(:,i), noiseStd,learnRate);
+        eigens(:,i) = SMhelper.genRandEigs(k,n,rho,'lognorm',100);
+        [dph,ephi,ave_rmsd] = rotaDiffConst(n,k, eigens(:,i), noiseStd,learnRate);
         allDiffConst(i,:) = [dph,ephi];
+        all_ave_rmsd{i} = ave_rmsd;
     end
 end
 
 % save the data for plot
 dataFile = [simType,'.mat'];
-save(dataFile,'noiseStd','eigens','allDiffConst','learnRate','step')
+save(dataFile,'noiseStd','eigens','allDiffConst','learnRate','step','all_ave_rmsd')
